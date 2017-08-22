ea82d5eccd8567a030489b6bf93e2fb728014e52
James Coleman <james.coleman@lm-c02t2544hf1r.inside.xero.com>
2017-08-22 11:35:02 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace XeroWinForm
+{
+    partial class XeroWinForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(439, 102);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run load to Xero";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(115, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 1;
+            // 
+            // XeroWinForm
+            // 
+            this.ClientSize = new System.Drawing.Size(1054, 478);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "XeroWinForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using Xero.Api.Core.Model;
+
+namespace XeroWinForm
+{
+    public partial class XeroWinForm : Form
+    {
+        public XeroWinForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /// first create an instance of the Xero API
+            var api = new Xero.Api.Example.Applications.Private.Core(false);
+
+            // create an instance of contact
+            Contact newContact = new Contact();
+            newContact.Name = "Metal Fencing"; // company name
+            newContact.FirstName = "John"; // first name
+            newContact.LastName = "Smith"; // last name
+            newContact.EmailAddress = "john@metalfencing.com"; // email
+
+            // call the API to create the contact
+            api.Contacts.Create(newContact);
+        }
+    }
+
+
+}
